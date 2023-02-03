2c743d9385284a593acff5558c81873a969a8efe
shadowasphodel2919 <t2905j@gmail.com>
2023-02-03 23:54:29 +0530

    Added stuff

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 //import logo from './logo.svg';
+import { Alphabet } from './Alphabet';
 import './App.css';
 import { Globe } from './Globe';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <h2>Taha Jamal</h2>
-      </header>
-      <section className='banner'>
+      </header> */}
+      {/* <section className='banner'>
         <Globe/>
-      </section>
-      <section className='menu'>
+      </section> */}
+      {/* <section className='menu'>
         <div className='sideTab'>About Me</div>
         <div className='sideTab'>Projects</div>
         <div className='sideTab'>Work</div>
-      </section>
+      </section> */}
+      {/* Adding side name */}
+      <Alphabet />
+        <div className='rightBanner'>
+          
+        </div>
     </div>
   );
 }
